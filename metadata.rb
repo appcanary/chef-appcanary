a6fedcb3cb3c548f12f8c60299f34bc8ba14fa25
Phill MV <phillmv@okayfail.com>
2015-09-22 10:50:57 -0400

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'hello@appcanary.com'
 license 'GPLv3'
 description 'Installs/Configures appcanary'
 long_description 'Installs/Configures appcanary'
-version '0.1.7'
+source_url       'https://github.com/appcanary/chef-appcanary'
+version '0.1.8'
 
 depends 'packagecloud', '~> 0.0.19'
